4288322dcd62912cb949430d930465030841e371
Christine Zhang <christineyiwen.zhang@gmail.com>
2025-02-10 10:26:40 -0500

    add link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,8 +15,8 @@
 
 <main>
     <h1>CHRISTINE Y. ZHANG</h1>
-    <p>I am an <a href="https://www.nytimes.com/by/christine-zhang">editing resident</a> in the graphics department at <b>The New York Times</b>. Previously, I was a data and graphics journalist at the <b>Financial Times</b>, the <b>Baltimore Sun</b> and the <b>Los Angeles Times</b>, and a research analyst at the <b>Brookings Institution</b>.</p>
-    <p>You can download my résumé <b><a href="czhang_resume_2023.pdf">here</a></b>.</p>
+    <p>I am a <a href="https://www.nytimes.com/by/christine-zhang">graphics editor</a> at <b>The New York Times</b>. Previously, I was a data and graphics journalist at the <b>Financial Times</b>, the <b>Baltimore Sun</b> and the <b>Los Angeles Times</b>, and a research analyst at the <b>Brookings Institution</b>.</p>
+    <p>You can download my résumé <b><a href="czhang_resume_2024.pdf">here</a></b>.</p>
     <p>Some of my work:</p>
 
     <div class="projects">
